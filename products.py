abe7542a228aea2a6f5c6b93a1b6d48398ab6b47
Minjun <didioooc12@gmail.com>
2020-09-08 22:40:29 +0800

    products.csv

--- products.py
+++ products.py
@@ -1,13 +1,18 @@
-#讓使用者可重複輸入商品名稱、價錢，並存入清單
-
 products = []
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q': #'q'表示結束輸入商品
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+
